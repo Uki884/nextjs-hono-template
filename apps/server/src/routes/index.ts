34a58fbbfed9e34b10f0a526b3702f86eb41a798
Uki884 <mrchildreny@gmail.com>
2025-01-05 00:05:27 +0900

    認証機能を追加し、APIルートに認証ミドルウェアを実装。依存関係を更新。

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -1,6 +1,38 @@
 import { Hono } from "hono";
 import { exampleRoutes } from "./example";
+import { authHandler, initAuthConfig, verifyAuth } from "@hono/auth-js";
+import Credentials from "@auth/core/providers/credentials";
+import { env } from 'hono/adapter';
 
-export const app = new Hono().basePath("/api");
+export const app = new Hono();
 
-export const apiRoutes = app.route("/examples", exampleRoutes);
+app.use(
+  '*',
+  initAuthConfig((c) => {
+    const { AUTH_SECRET } = env<{ AUTH_SECRET: string }>(c)
+    return {
+      secret: AUTH_SECRET,
+        providers: [
+            Credentials({
+            credentials: {
+              username: { label: "Username" },
+              password: { label: "Password", type: "password" },
+            },
+            async authorize({ username, password }) {
+              console.log('username', username, password);
+              if (username === "admin" && password === "admin") {
+                return { id: "admin", username: "admin" };
+              }
+              throw new Error("Invalid credentials");
+            },
+          }),
+        ],
+  }
+  })
+)
+
+app.use('/api/auth/*', authHandler());
+
+app.use('/api/*', verifyAuth())
+
+export const apiRoutes = app.basePath('/api').route("/examples", exampleRoutes);
